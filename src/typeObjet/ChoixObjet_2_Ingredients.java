c8acc65a2fac75d945366d78ae79fe5f5e002242
Alexandre Stoppini <stoppini.alexandre@gmail.com>
2020-04-24 21:51:46 +0200

    ajout de choix objet 2 ingrédients

--- src/typeObjet/ChoixObjet_2_Ingredients.java
+++ src/typeObjet/ChoixObjet_2_Ingredients.java
@@ -1,4 +1,116 @@
 package typeObjet;
 
+import java.util.Scanner;
+
 public class ChoixObjet_2_Ingredients {
+    private static Scanner sc = new Scanner(System.in);
+    public static void ChoixTypeObjet2() {
+        System.out.println("Quel objet souhaitez-vous crafter ?" +
+                "\n1/ reservoir" +
+                "\n2/ convoyeur_jaune" +
+                "\n3/ convoyeur_rouge" +
+                "\n4/ convoyeur_jaune_souterrain" +
+                "\n5/ convoyeur_rouge_souterrain" +
+                "\n6/ bras_robotise_filtrant" +
+                "\n7/ bras_robotise_filtrant_haute_capacite" +
+                "\n8/ petit_poteau_electrique" +
+                "\n9/ tuyau_souterrain" +
+                "\n10/ signal_rail" +
+                "\n11/ signal_rail_chaine" +
+                "\n12/ robot_logistique" +
+                "\n13/ robot_combat" +
+                "\n14/ fil_rouge" +
+                "\n15/ fil_vert" +
+                "\n16/ calculateur" +
+                "\n17/ comparateur" +
+                "\n18/ emetteur_de_constante" +
+                "\n19/ pack_reparation" +
+                "\n20/ accumulateur" +
+                "\n21/ conduite_de_chaleur" +
+                "\n22/ machine_assemblage_3" +
+                "\n23/ module_vitesse_1" +
+                "\n24/ module_efficacite_1" +
+                "\n25/ module_productivite_1" +
+                "\n26/ barre_plastique" +
+                "\n27/ souffre" +
+                "\n28/ circuit_electronique" +
+                "\n29/ controle_fusée" +
+                "\n30/ rocket_fuel" +
+                "\n31/ combustible_nucleaire" +
+                "\n32/ science_pack_rouge" +
+                "\n33/ science_pack_vert");
+    }
+
+    public static Objets_2_Ingredients typeObjet(){
+        while (true){
+            String choixObj = sc.next();
+            if (choixObj.contains("1")){
+                return Objets_2_Ingredients.reservoir;
+            } else if (choixObj.contains("2")){
+                return Objets_2_Ingredients.convoyeur_jaune;
+            }else if (choixObj.contains("3")){
+                return Objets_2_Ingredients.convoyeur_rouge;
+            }else if (choixObj.contains("4")){
+                return Objets_2_Ingredients.convoyeur_jaune_souterrain;
+            }else if (choixObj.contains("5")){
+                return Objets_2_Ingredients.convoyeur_rouge_souterrain;
+            }else if (choixObj.contains("6")){
+                return Objets_2_Ingredients.bras_robotise_filtrant;
+            }else if (choixObj.contains("7")){
+                return Objets_2_Ingredients.bras_robotise_filtrant_haute_capacite;
+            }else if (choixObj.contains("8")){
+                return Objets_2_Ingredients.petit_poteau_electrique;
+            }else if (choixObj.contains("9")){
+                return Objets_2_Ingredients.tuyau_souterrain;
+            }else if (choixObj.contains("10")){
+                return Objets_2_Ingredients.signal_rail;
+            }else if (choixObj.contains("11")){
+                return Objets_2_Ingredients.signal_rail_chaine;
+            }else if (choixObj.contains("12")){
+                return Objets_2_Ingredients.robot_logistique;
+            }else if (choixObj.contains("13")){
+                return Objets_2_Ingredients.robot_combat;
+            }else if (choixObj.contains("14")){
+                return Objets_2_Ingredients.fil_rouge;
+            }else if (choixObj.contains("15")){
+                return Objets_2_Ingredients.fil_vert;
+            }else if (choixObj.contains("16")){
+                return Objets_2_Ingredients.calculateur;
+            }else if (choixObj.contains("17")){
+                return Objets_2_Ingredients.comparateur;
+            }else if (choixObj.contains("18")){
+                return Objets_2_Ingredients.emetteur_de_constante;
+            }else if (choixObj.contains("19")){
+                return Objets_2_Ingredients.pack_reparation;
+            }else if (choixObj.contains("20")){
+                return Objets_2_Ingredients.accumulateur;
+            }else if (choixObj.contains("21")){
+                return Objets_2_Ingredients.conduite_de_chaleur;
+            }else if (choixObj.contains("22")){
+                return Objets_2_Ingredients.machine_assemblage_3;
+            }else if (choixObj.contains("23")){
+                return Objets_2_Ingredients.module_vitesse_1;
+            }else if (choixObj.contains("24")){
+                return Objets_2_Ingredients.module_efficacite_1;
+            }else if (choixObj.contains("25")){
+                return Objets_2_Ingredients.module_productivite_1;
+            }else if (choixObj.contains("26")){
+                return Objets_2_Ingredients.barre_plastique;
+            }else if (choixObj.contains("27")){
+                return Objets_2_Ingredients.souffre;
+            }else if (choixObj.contains("28")){
+                return Objets_2_Ingredients.circuit_electronique;
+            }else if (choixObj.contains("29")){
+                return Objets_2_Ingredients.controle_fusée;
+            }else if (choixObj.contains("30")){
+                return Objets_2_Ingredients.rocket_fuel;
+            }else if (choixObj.contains("31")) {
+                return Objets_2_Ingredients.combustible_nucleaire;
+            }else if (choixObj.contains("32")) {
+                return Objets_2_Ingredients.science_pack_rouge;
+            }else if (choixObj.contains("33")) {
+                return Objets_2_Ingredients.science_pack_vert;
+            }
+        }
+    }
 }
--- src/typeObjet/Objets_2_Ingredients.java
+++ src/typeObjet/Objets_2_Ingredients.java
@@ -1,34 +1,39 @@
 package typeObjet;
 
 public enum Objets_2_Ingredients {
-    reservoir(3.0,20,5,1),
-    convoyeur_jaune(0.5,1,1,2),
-    convoyeur_rouge(0.5,5,1,1),
-    convoyeur_jaune_souterrain(1.0,10,5,2),
-    convoyeur_rouge_souterrain(2.0,40,2,2),
-    bras_robotise_filtrant(0.5,4,1,1),
-    bras_robotise_filtrant_haute_capacite(0.5,5,1,1),
-    petit_poteau_electrique(0.5,2,1,2),
-    tuyau_souterrain(0.5,5,10,2),
-    signal_rail(0.5,1,5,1),
-    signal_rail_chaine(0.5,1,5,1),
-    robot_logistique(0.5,2,1,1),
-    robot_combat(0.5,2,1,1),
-    fil_rouge(0.5,1,1,1),
-    fil_vert(0.5,1,1,1),
-    calculateur(0.5,5,5,1),
-    comparateur(0.5,5,5,1),
-    emetteur_de_constante(0.5,5,2,1),
-    pack_reparation(0.5,2,2,1),
-    accumulateur(10.0,5,2,1),
-    conduite_de_chaleur(1.0,20,10,1),
-    machine_assemblage_3(0.5,2,4,1),
-    module_vitesse_1(15.0,5,5,1),
-    module_efficacite_1(15.0,5,5,1),
-    module_productivite_1(15.0,5,5,1),
-    barre_plastique(1.0,1,20,2),
-    souffre(1.0,30,30,2),
-
+    reservoir(3.0, 20, 5, 1),
+    convoyeur_jaune(0.5, 1, 1, 2),
+    convoyeur_rouge(0.5, 5, 1, 1),
+    convoyeur_jaune_souterrain(1.0, 10, 5, 2),
+    convoyeur_rouge_souterrain(2.0, 40, 2, 2),
+    bras_robotise_filtrant(0.5, 4, 1, 1),
+    bras_robotise_filtrant_haute_capacite(0.5, 5, 1, 1),
+    petit_poteau_electrique(0.5, 2, 1, 2),
+    tuyau_souterrain(0.5, 5, 10, 2),
+    signal_rail(0.5, 1, 5, 1),
+    signal_rail_chaine(0.5, 1, 5, 1),
+    robot_logistique(0.5, 2, 1, 1),
+    robot_combat(0.5, 2, 1, 1),
+    fil_rouge(0.5, 1, 1, 1),
+    fil_vert(0.5, 1, 1, 1),
+    calculateur(0.5, 5, 5, 1),
+    comparateur(0.5, 5, 5, 1),
+    emetteur_de_constante(0.5, 5, 2, 1),
+    pack_reparation(0.5, 2, 2, 1),
+    accumulateur(10.0, 5, 2, 1),
+    conduite_de_chaleur(1.0, 20, 10, 1),
+    machine_assemblage_3(0.5, 2, 4, 1),
+    module_vitesse_1(15.0, 5, 5, 1),
+    module_efficacite_1(15.0, 5, 5, 1),
+    module_productivite_1(15.0, 5, 5, 1),
+    barre_plastique(1.0, 1, 20, 2),
+    souffre(1.0, 30, 30, 2),
+    circuit_electronique(0.5, 3, 1, 1),
+    controle_fusée(30.0, 1, 1, 1),
+    rocket_fuel(30.0, 10, 10, 1),
+    combustible_nucleaire(90.0, 1, 1, 1),
+    science_pack_rouge(5.0, 1, 1, 1),
+    science_pack_vert(6.0, 1, 1, 1),
     ;
 
     private double timeToCraft;
@@ -39,7 +44,7 @@ public enum Objets_2_Ingredients {
     Objets_2_Ingredients(Double timeToCraft, int nbRessourceForCraft_1, int nbRessourceForCraft_2, int nbItemCraft) {
         this.timeToCraft = timeToCraft;
         this.nbRessourceForCraft_1 = nbRessourceForCraft_1;
-        this.nbRessourceForCraft_2 =nbRessourceForCraft_2;
+        this.nbRessourceForCraft_2 = nbRessourceForCraft_2;
         this.nbItemCraft = nbItemCraft;
     }
 
@@ -47,7 +52,13 @@ public enum Objets_2_Ingredients {
         return timeToCraft;
     }
 
+    public int getNbRessourceForCraft_1() {
+        return nbRessourceForCraft_1;
+    }
 
+    public int getNbRessourceForCraft_2() {
+        return nbRessourceForCraft_2;
+    }
 
     public int getNbItemCraft() {
         return nbItemCraft;
